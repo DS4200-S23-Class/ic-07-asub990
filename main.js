a71a7975f36a646ecbb9ae2ea04408a7850df848
Adithi Subramaniam <adithisubramaniam@Adithis-MacBook-Pro.local>
2023-02-15 15:29:28 -0500

    adding updates

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const margins = {left: 50, right: 50, top:50, bottom: 50};
+const MARGINS = {left: 50, right: 50, top:50, bottom: 50};
+const FRAME_HEIGHT = 500;
+const FRAME_WIDTH = 500;
 const DATA = [55000, 48000, 27000, 66000, 90000];
 
 // scaling functions
@@ -8,9 +10,16 @@ const X_SCALE = d3.scaleLinear().domain([0, (MAX_X + 10000)]).range(0, 1000);
 // add axis
 
 // create a frame
-const frame = d3.select("plot").append("svg").attr("height", 500).attr("width", 500).attr("class", "frame");
+const FRAME = d3.select("vis1")
+              .append("svg")
+              .attr("height", FRAME_HEIGHT)
+              .attr("width", FRAME_WIDTH)
+              .attr("class", "frame");
 
 // add data to the frame
-frame.selectAll("points").data(DATA).enter().append("cirle").attr("cx", 50).attr("cy", (d) => {return (X_SCALE(d) + margins.left)}).attr("r", 20)
-.attr("r", 5).attr("class", "point");
+FRAME.selectAll("points").data(DATA).enter().append("cirle")
+.attr("cx", 50)
+.attr("cy", (d) => {return (X_SCALE(d) + MARGINS.left)})
+.attr("r", 5)
+.attr("class", "point");
 
